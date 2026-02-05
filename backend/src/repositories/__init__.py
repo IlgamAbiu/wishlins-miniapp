776895e050300223574fcc8bc6333c06da99ca49
Akhmatdinov Ilgam <ilgamfa@yandex.ru>
2026-02-06 00:00:46 +0300

    fix: remove invalid import in backend repositories

--- backend/src/repositories/__init__.py
+++ backend/src/repositories/__init__.py
@@ -1,6 +1,6 @@
 from .user_repository import UserRepository
 from .wishlist_repository import WishlistRepository
-from .wish_request import WishRequestRepository
+
 from .wish import WishRepository
 
 __all__ = ["UserRepository", "WishlistRepository", "WishRepository"]
